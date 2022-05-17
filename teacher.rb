13600155388c443f93138c1ac649d5369f47a15e
Saadat Ali <saadatlucky313@gmail.com>
2022-05-17 23:07:31 +0500

    Create Teacher Class

---
+++ teacher.rb
@@ -0,0 +1,12 @@
+require './Person'
+
+class Teacher < Person
+  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
+    @specialization = specialization
+    super(name, age, parent_permission: parent_permission)
+  end
+
+  def can_use_services?
+    true
+  end
+end
