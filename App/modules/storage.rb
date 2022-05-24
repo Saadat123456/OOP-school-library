e27afd47ed262d725340d38de0410e5dc0c21c45
Saadat Ali <saadatlucky313@gmail.com>
2022-05-25 04:08:46 +0500

    Create Load Data From Json Function

--- App/modules/storage.rb
+++ App/modules/storage.rb
@@ -1,14 +1,27 @@
 require 'json'
 
 class Storage
+  def initialize
+    @base_path = './storage/'
+    @ext = '.json'
+  end
+
   def save_data(class_name, data)
     obj = []
     data.each do |o|
       obj.push(o.to_json)
     end
-    path = "./storage/#{class_name}.json"
+    path = "#{@base_path}#{class_name}#{@ext}"
     File.open(path, 'w') do |file|
       JSON.dump(obj, file)
+    end
+  end
+
+  def load_data(class_name)
+    path = "#{@base_path}#{class_name}#{@ext}"
+    File.open(path) do |file|
+      data = file.read
+      return JSON.parse(data)
     end
   end
 end
