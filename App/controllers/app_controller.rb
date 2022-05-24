456cc69691879acc2088bfe7384eb54a3acb77a3
Saadat Ali <saadatlucky313@gmail.com>
2022-05-25 04:08:26 +0500

    Load Data From Json

--- App/controllers/app_controller.rb
+++ App/controllers/app_controller.rb
@@ -3,6 +3,10 @@ require_relative '../modules/books'
 require_relative '../modules/people'
 require_relative '../modules/rentals'
 require_relative '../modules/storage'
+require_relative '../../book'
+require_relative '../../student'
+require_relative '../../teacher'
+require 'pry'
 
 class AppController < AppControllerInterface
   def initialize
@@ -11,6 +15,22 @@ class AppController < AppControllerInterface
     @people = People.new
     @rentals = Rentals.new(@books, @people)
     @storage = Storage.new
+    books_array = @storage.load_data('books')
+    books_array.each do |book|
+      book_obj = JSON.parse(book)
+      @books.books << Book.new(book_obj['title'], book_obj['author'])
+    end
+    people_array = @storage.load_data('people')
+    people_array.each do |person|
+      person_obj = JSON.parse(person)
+      if person_obj.key?('specialization')
+        @people.people << Teacher.new(person_obj['age'], person_obj['specialization'], person_obj['name'], parent_permission: person_obj['parent_permission'])
+      else
+        @people.people << Student.new(person_obj['age'], person_obj['specialization'], person_obj['name'],
+          parent_permission: person_obj['parent_permission'])
+      end
+    end
+    binding.pry
   end
 
   def run_user_command(option)
