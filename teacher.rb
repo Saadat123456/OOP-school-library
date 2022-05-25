37bf1b160f4ce1a737a93976beef2ef565c1f533
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-05-25 15:44:58 -0500

    Add tests for Rental, Teacher & Classroom classes

--- teacher.rb
+++ teacher.rb
@@ -2,6 +2,8 @@ require_relative './person'
 require 'json'
 
 class Teacher < Person
+  attr_reader :specialization
+
   def initialize(age, specialization, name = 'Unknown', parent_permission: true)
     @specialization = specialization
     super(age, name, parent_permission: parent_permission)
