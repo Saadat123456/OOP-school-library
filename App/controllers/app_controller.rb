7e1b62556d3abca474fe8b5c8dc8d90acee709ef
Saadat Ali <saadatlucky313@gmail.com>
2022-05-26 00:35:23 +0500

    Load All Data From Storage

--- App/controllers/app_controller.rb
+++ App/controllers/app_controller.rb
@@ -6,7 +6,6 @@ require_relative '../modules/storage'
 require_relative '../../book'
 require_relative '../../student'
 require_relative '../../teacher'
-require 'pry'
 
 class AppController < AppControllerInterface
   def initialize
@@ -17,20 +16,16 @@ class AppController < AppControllerInterface
     @storage = Storage.new
     books_array = @storage.load_data('books')
     books_array.each do |book|
-      book_obj = JSON.parse(book)
-      @books.books << Book.new(book_obj['title'], book_obj['author'])
+      @books.books << JSON.parse(book, create_additions: true)
     end
     people_array = @storage.load_data('people')
     people_array.each do |person|
-      person_obj = JSON.parse(person)
-      if person_obj.key?('specialization')
-        @people.people << Teacher.new(person_obj['age'], person_obj['specialization'], person_obj['name'], parent_permission: person_obj['parent_permission'])
-      else
-        @people.people << Student.new(person_obj['age'], person_obj['specialization'], person_obj['name'],
-          parent_permission: person_obj['parent_permission'])
-      end
+      @people.people << JSON.parse(person, create_additions: true)
     end
-    binding.pry
+    rentals_array = @storage.load_data('rentals')
+    rentals_array.each do |rental|
+      @rentals.rentals << JSON.parse(rental, create_additions: true)
+    end
   end
 
   def run_user_command(option)
